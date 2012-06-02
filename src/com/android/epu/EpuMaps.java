2ca6dd695a674c84d548cde8403a4f5d40a44f52
Federico De Meo <demeof@gmail.com>
2012-06-02 17:01:46 +0200

    Starting EpuMaps activity.

---
+++ src/com/android/epu/EpuMaps.java
@@ -0,0 +1,13 @@
+package com.android.epu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EpuMaps extends Activity {
+
+  
+  public void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.maps);
+ }
+}
