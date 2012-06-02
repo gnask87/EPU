00af5aa69721f00f0aefa2dcf08b4ed6eaf42405
Federico De Meo <demeof@gmail.com>
2012-06-02 13:47:33 +0200

    Fist commit

---
+++ src/com/android/epu/EpuActivity.java
@@ -0,0 +1,13 @@
+package com.android.epu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EpuActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
