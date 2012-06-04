ad446cf7378aea59eeab1734d246d85d11112c13
Federico De Meo <demeof@gmail.com>
2012-06-04 13:17:48 +0200

    cleaning

--- src/com/android/epu/Utils.java
+++
@@ -1,9 +0,0 @@
-package com.android.epu;
-
-public class Utils {
-  
-  public static String hq_host = "http://tizipic.mine.nu/";
-  public static String tags = "tags";
-  public static String upload = "upload/";
-  
-}
